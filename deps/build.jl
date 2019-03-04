ed7ac7c61477f39f9d6443f4578c1210682ff5da
D <djp42@stanford.edu>
2019-03-03 17:03:59 -0800

    update build

--- deps/build.jl
+++ deps/build.jl
@@ -1,9 +1,10 @@
-# clone
+using Pkg
+
+# unregistered packages
 urls = [
     "https://github.com/sisl/Vec.jl.git",
     "https://github.com/sisl/AutomotiveDrivingModels.jl.git",
     "https://github.com/sisl/AutoViz.jl.git",
-    "https://github.com/tawheeler/ForwardNets.jl.git",
     "https://github.com/sisl/BayesNets.jl.git",
     "https://github.com/sisl/GridInterpolations.jl.git",
 ]
@@ -11,27 +12,11 @@ urls = [
 packages = keys(Pkg.installed())
 for url in urls
     try
-        id1 = search(url, "https://github.com/")[end]
-        offset = search(url[(id1[end]+1):end], "/")[end]
-        package = url[(id1+offset+1): (search(url,".jl.git")[1]-1)]
-        if !in(package, packages)
-          Pkg.clone(url)
-        else
-          println("$(package) already exists. Not cloning.")
-        end
+  	Pkg.add(PackageSpec(url=url))
     catch e
         println("Exception when cloning $(url): $(e)")
     end
 end
 
-Pkg.build("AutomotiveDrivingModels")
-Pkg.build("BayesNets")
-
-# checkout specific branches
-checkouts = [
-    ("ForwardNets", "nextgen")
-]
-
-for (pkg, branch) in checkouts
-    Pkg.checkout(pkg, branch)
-end
+Pkg.add(PackageSpec(url="https://github.com/tawheeler/ForwardNets.jl.git",
+    rev="nextgen"))
